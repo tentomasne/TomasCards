c44c1bf39a5df33783ff823e9debd9e404f27eea
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2025-06-29 16:30:57 +0200

    sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,8 +10,24 @@ import { loadSettings } from '@/utils/storage';
 import { debugManager } from '@/utils/debugManager';
 import SplashScreen from '@/components/SplashScreen';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://2ec3ab03dfca243b946e4e937a2a5288@o4507349849079808.ingest.de.sentry.io/4509582407630929',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+});
+
+export default Sentry.wrap(function RootLayout() {
   useFrameworkReady();
   const [isLoading, setIsLoading] = useState(true);
   const [loadingMessage, setLoadingMessage] = useState('');
@@ -57,7 +73,7 @@ export default function RootLayout() {
       </ThemeProvider>
     </SafeAreaView>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
